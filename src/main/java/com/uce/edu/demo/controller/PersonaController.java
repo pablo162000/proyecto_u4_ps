e71a3b671d41353b83fb486fca7fc2b2d8b574aa
Pablo Suntaxi <pbsun.ale@gmail.com>
2022-09-05 20:57:34 -0500

    TALLER 38: Programación Web - GET, POST, PUT y DELETE

--- src/main/java/com/uce/edu/demo/controller/PersonaController.java
+++ src/main/java/com/uce/edu/demo/controller/PersonaController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.uce.edu.demo.repository.modelo.Persona;
@@ -14,17 +18,57 @@ import com.uce.edu.demo.service.IPersonaService;
 @Controller
 @RequestMapping("/personas")
 public class PersonaController {
-	
+
 	@Autowired
 	private IPersonaService personaService;
-	
-	//GET
+
+	// GET
 	@GetMapping("/buscar")
-	public String buscarTodos(Model modelo){
+	public String buscarTodos(Model modelo) {
 		List<Persona> lista = this.personaService.buscarTodos();
-		modelo.addAttribute("personas",lista);
-		return "vistaListaEstudiantes";
+		modelo.addAttribute("personas", lista);
+		return "vistaListaPersonas";
+	}
+
+	@GetMapping("/buscarUno/{idPersona}")
+	public String buscarEmpleado(@PathVariable("idPersona") Integer id, Model modelo) {
+
+		System.out.println("El ID:" + id);
+		Persona per = this.personaService.buscarPorId(id);
+
+		modelo.addAttribute("persona", per);
+
+		return "vistaPersona";
+	}
+
+	@PutMapping("/actualizar/{idPersona}")
+	public String actualizarPersona(@PathVariable("idPersona") Integer id, Persona persona) {
+		persona.setId(id);
+		this.personaService.actualizar(persona);
+		return "redirect:/personas/buscar";
+	}
+
+@DeleteMapping("/borrar/{idPersona}")
+	public String borrarPersona(@PathVariable("idPersona") Integer id) {
+
+		this.personaService.eliminar(id);
+		return "redirect:/personas/buscar";
+	}
+
+
+	
+	
+	
+	@PostMapping("/insertar")
+	public String insertarPersona(Persona persona) {
+		this.personaService.guardar(persona);
+		return "redirect:/personas/buscar";
 	}
 	
+	// Métodos de redireccionamientos a páginas
+	@GetMapping("/nuevaPersona")
+	public String paginaNuevaPersona(Persona persona) {
+		return "vistaNuevaPersona";
+	}
 
 }
